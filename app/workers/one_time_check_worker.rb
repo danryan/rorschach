4322e7215c6ba3fa79c88cf40bde32fc4408f894
Dan Ryan <scriptfu@gmail.com>
2012-08-11 01:49:27 -0400

    allow schedule of one-time checks

---
+++ app/workers/one_time_check_worker.rb
@@ -0,0 +1,43 @@
+class OneTimeCheckWorker
+  include Sidekiq::Worker
+
+  def perform(id)
+    check = Check.find(id)
+    
+    metric = check.metric
+    value = check.get_value
+    warning = check.warning
+    critical = check.critical
+    last_state = check.state
+    
+    if value
+      if critical > warning
+        current_state = case 
+          when value >= critical
+            'critical'
+          when value >= warning
+            'warning'
+          else
+            'ok'
+        end
+      else
+        current_state = case
+          when value <= critical
+            'critical'
+          when value <= warning
+            'warning'
+          else
+            'ok'
+        end
+      end
+    else
+      current_state = 'undefined'
+    end
+    
+    check.update_attributes(last_value: value)
+    
+    if current_state != last_state
+      check.send("set_#{current_state}")
+    end    
+  end
+end
