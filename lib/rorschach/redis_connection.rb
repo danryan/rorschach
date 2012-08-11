dae80ae2d8f8dd1c3ffabda76c1952998bcb06a1
Dan Ryan <scriptfu@gmail.com>
2012-08-11 01:47:32 -0400

    add redis client

---
+++ lib/rorschach/redis_connection.rb
@@ -0,0 +1,28 @@
+require 'connection_pool'
+require 'redis'
+require 'redis/namespace'
+
+module Rorschach
+  class RedisConnection
+    def self.create(options={})
+      url = options[:url] || ENV['REDISTOGO_URL'] || 'redis://localhost:6379/0'
+      driver = options[:driver] || 'ruby'
+      # need a connection for Fetcher and Retry
+      size = options[:size] || 5
+
+      ConnectionPool.new(:timeout => 1, :size => size) do
+        build_client(url, options[:namespace], driver)
+      end
+    end
+
+    def self.build_client(url, namespace, driver)
+      client = Redis.connect(:url => url, :driver => driver)
+      if namespace
+        Redis::Namespace.new(namespace, :redis => client)
+      else
+        client
+      end
+    end
+    private_class_method :build_client
+  end
+end
